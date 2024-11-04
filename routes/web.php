737f94d80c7bd778fa990b10884bf9a022834807
Alexaweb <alexaweb999@gmail.com>
2024-11-04 08:20:31 +0100

    v 0.0.2  anadir login.blade

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
+Route::post('/login', function () {
+    return view('welcome');
+}) ->name('login');
